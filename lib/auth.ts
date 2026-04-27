02ea48958a69f5d092f445da200afaa9b727d080
Adam Haluska <adam.haluska3@gmail.com>
2026-04-27 17:59:52 +0200

    Add OAuth login

--- lib/auth.ts
+++ lib/auth.ts
@@ -14,9 +14,6 @@ export const auth = betterAuth({
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID ?? "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      accessType: "offline",
-      prompt: "consent",
-      scope: [],
     },
   },
   user: {
