aa5d56386697e2272c6be9da33846305eb4780c2
radobanik <mail@radobanik.com>
2026-05-10 15:37:14 +0200

    Create booking only with selected user

--- components/admin/booking-dialog.tsx
+++ components/admin/booking-dialog.tsx
@@ -422,7 +422,7 @@ export function BookingDialog({
             <Button variant="outline" onClick={onClose}>
               Zrušiť
             </Button>
-            <Button onClick={handleSave}>
+            <Button onClick={handleSave} disabled={!isEdit && !selectedUserId}>
               {isEdit ? "Uložiť" : "Vytvoriť"}
             </Button>
           </DialogFooter>
