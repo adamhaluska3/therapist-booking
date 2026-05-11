2e95336ca0d4ebef3e1815d46d66908b238464d6
Adam Haluska <adam.haluska3@gmail.com>
2026-05-11 08:02:26 +0200

    Extend middleware with /clients

--- middleware.tsx
+++ middleware.tsx
@@ -6,7 +6,16 @@ export const runtime = "nodejs";
 export async function middleware(request: NextRequest) {
   const session = await auth.api.getSession({ headers: request.headers });
 
-  if (!session?.user || session.user.role !== "admin") {
+  if (!session?.user) {
+    return NextResponse.redirect(
+      new URL("/auth-error?reason=unauthorized", request.url),
+    );
+  }
+
+  if (
+    request.nextUrl.pathname.startsWith("/admin") &&
+    session.user.role !== "admin"
+  ) {
     return NextResponse.redirect(
       new URL("/auth-error?reason=forbidden", request.url),
     );
@@ -16,5 +25,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/admin/:path*"],
+  matcher: ["/admin/:path*", "/client/:path*"],
 };
